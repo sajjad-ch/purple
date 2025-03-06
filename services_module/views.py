06a0d5bbd3b4d8c5c7968002792c54427c25d78b
unknown <sajjadbad2002@gmail.com>
2025-03-06 12:39:28 +0330

    changin time for story

--- services_module/views.py
+++ services_module/views.py
@@ -732,7 +732,7 @@ class StoryAPIView(APIView):
                     # Remove the temporary file
                     os.remove(temp_file_path)
 
-                    if duration > 10:
+                    if duration > 30:
                         return Response({'error': 'Video duration should be less than 10 seconds.'},
                                         status=status.HTTP_400_BAD_REQUEST)
                     # if width != 600 or height != 1068:
