5f2a78b2631d605a3cbf561fd439ce5955dd8e20
unknown <sajjadbad2002@gmail.com>
2025-03-07 15:09:40 +0330

    adding caches storage for prefetch reloading

--- purple/settings.py
+++ purple/settings.py
@@ -99,6 +99,13 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 # CHANNEL_LAYERS = {
 #     "default": {
 #         "BACKEND": "channels.layers.InMemoryChannelLayer",
