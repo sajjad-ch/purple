98386db5acf6893084342f98ec72a1456b7b643d
unknown <sajjadbad2002@gmail.com>
2025-05-06 00:13:19 +0330

    ADDING a response to the visiting view

--- services_module/views.py
+++ services_module/views.py
@@ -1110,7 +1110,7 @@ class RequestVisitingTimeSaloonAPIView(APIView):
             url = "http://127.0.0.1:8000/service/visits/"
             # sms_for_new_visiting_time_saloon(saloon_obj.saloon.phone_number, saloon_obj.saloon.first_name) # TODO: Uncomment the notification function  
             # sms_for_new_visiting_time_artist(artist.artist.phone_number, artist.artist.first_name)   # TODO: Uncomment the notification function              return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+            return Response('Appointment Created', status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
