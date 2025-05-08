a850167b6378648f0a9a9b303c68cc28e05a5539
unknown <sajjadbad2002@gmail.com>
2025-05-08 14:56:52 +0330

    change

--- services_module/views.py
+++ services_module/views.py
@@ -1356,7 +1356,7 @@ class PostConfirmVisitAPIView(APIView):
                 phone_number = visit.user.phone_number
                 sms_for_result_of_appointment(phone_number, 'تایید', visit_id)     # TODO: UNcomment this function
                 paying_url = ''
-                sms_for_reminding_deposit(phone_number, paying_url, visit.saloon.saloon.first_name, visit.artist.artist.first_name, visit_id)     # TODO: UNcomment this function
+                sms_for_reminding_deposit(phone_number, paying_url, visit.saloon.saloon.first_name, visit.artist.artist.first_name)     # TODO: UNcomment this function
                 message = "نوبت شما تایید شد برای پرداخت بیعانه 40 دقیقه وقت دارید."
                 url = "http://127.0.0.1:8000/service/visits/payment/"
                 # send_verification_code(message, phone_number, url)
