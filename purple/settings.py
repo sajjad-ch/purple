9a3989e7cec144aa213263e2681584b505a0b750
unknown <sajjadbad2002@gmail.com>
2025-05-11 18:03:20 +0330

    add the app.banafshapp.ir to the allowed hosts

--- purple/settings.py
+++ purple/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-90xmq_rm1dl9g2cvwbtfa@+96yg@yjhi=vlq+%uz9%8v-j7#2z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["212.23.201.20", '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["212.23.201.20", '127.0.0.1', 'localhost', 'app.banafshapp.ir']
 
 
 # Application definition
