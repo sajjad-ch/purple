2cdaf474c582c6b15d55547fcc0205d5bb503791
unknown <sajjadbad2002@gmail.com>
2025-05-16 17:14:59 +0330

    change the middleware

--- ChatAPI/middleware.py
+++ ChatAPI/middleware.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.conf import settings
 from channels.middleware import BaseMiddleware
 from rest_framework_simplejwt.tokens import AccessToken
-
+from urllib.parse import parse_qs
 
 @database_sync_to_async
 def get_user_from_jwt(token):
@@ -24,41 +24,81 @@ def get_user_from_jwt(token):
         return AnonymousUser()
     
 
-class JWTAuthMiddleware(BaseMiddleware):
+# class JWTAuthMiddleware(BaseMiddleware):
     
-    async def __call__(self, scope, receive, send):
+#     async def __call__(self, scope, receive, send):
     
-        token = self.get_token_from_scope(scope)
+#         token = self.get_token_from_scope(scope)
         
-        if token != None:
-            user_id = await self.get_user_from_token(token) 
-            if user_id:
-                scope['user_id'] = user_id
+#         if token != None:
+#             user_id = await self.get_user_from_token(token) 
+#             if user_id:
+#                 scope['user_id'] = user_id
 
-            else:
-                scope['error'] = 'Invalid token'
+#             else:
+#                 scope['error'] = 'Invalid token'
 
-        if token == None:
-            scope['error'] = 'provide an auth token'    
+#         if token == None:
+#             scope['error'] = 'provide an auth token'    
     
                 
+#         return await super().__call__(scope, receive, send)
+
+#     def get_token_from_scope(self, scope):
+#         headers = dict(scope.get("headers", []))
+        
+#         auth_header = headers.get(b'authorization', b'').decode('utf-8')
+        
+#         if auth_header.startswith('Bearer '):
+#             return auth_header.split(' ')[1]
+        
+#         else:
+#             return None
+        
+#     @database_sync_to_async
+#     def get_user_from_token(self, token):
+#             try:
+#                 access_token = AccessToken(token)
+#                 return access_token['user_id']
+#             except:
+#                 return None
+
+
+class JWTAuthMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        token = self.get_token_from_scope(scope)
+
+        if token is not None:
+            user_id = await self.get_user_from_token(token)
+            if user_id:
+                scope['user_id'] = user_id
+            else:
+                scope['error'] = 'Invalid token'
+        else:
+            scope['error'] = 'Provide an auth token'
+
         return await super().__call__(scope, receive, send)
 
     def get_token_from_scope(self, scope):
+        # Check query string first (for PWA case)
+        query_string = scope.get('query_string', b'').decode()
+        query_params = parse_qs(query_string)
+
+        if 'token' in query_params:
+            return query_params['token'][0]
+
+        # Fallback to Authorization header (for non-PWA clients)
         headers = dict(scope.get("headers", []))
-        
         auth_header = headers.get(b'authorization', b'').decode('utf-8')
-        
         if auth_header.startswith('Bearer '):
             return auth_header.split(' ')[1]
-        
-        else:
-            return None
-        
+
+        return None
+
     @database_sync_to_async
     def get_user_from_token(self, token):
-            try:
-                access_token = AccessToken(token)
-                return access_token['user_id']
-            except:
-                return None
+        try:
+            access_token = AccessToken(token)
+            return access_token['user_id']
+        except Exception:
+            return None
