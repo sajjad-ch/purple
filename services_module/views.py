acb8adef6f338899b8a1f79eca0df9752da4bf6a
unknown <sajjadbad2002@gmail.com>
2025-05-21 14:43:27 +0330

    fix the bug

--- services_module/views.py
+++ services_module/views.py
@@ -1525,9 +1525,9 @@ class ChangeConfirmedToCompleted(APIView):
 
                     if timezone.is_naive(visit_end_time):
                         visit_end_time = timezone.make_aware(visit_end_time)
-                    logger.info(f'end time: {visit_end_time}')
-                    logger.info(f'current time: {current_time}')
-                    logger.info(f'exact time: {visit.exact_time}')
+                    logger.info(f'end time: {visit_end_time}, type:{type(visit_end_time)}')
+                    logger.info(f'current time: {current_time}, type: {current_time}')
+                    logger.info(f'exact time: {visit.exact_time}, type: {visit.exact_time}')
                     if visit_end_time <= current_time:
                         visit.status = 'completed'
                         visit.save()
